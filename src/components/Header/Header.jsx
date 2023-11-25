7ce80019064a7defdd4587459f714ca8403899fa
jameelag1995 <agbariajameel@gmail.com>
2023-11-25 19:59:29 +0200

    reload page on logo click

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "./Header.css";
 import TabButton from "./TabButton";
 export default function Header() {
+    function reloadPage(){
+        window.location.reload();
+    }
     return <nav className="header-container">
-        <div className="logo"></div>
+        <div className="logo" onClick={reloadPage}></div>
         <menu>
             <TabButton id='projects'>Projects</TabButton>
             <TabButton id='about'>About</TabButton>
